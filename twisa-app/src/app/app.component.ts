92886d703f4d1edf56f524ff06ed8ded90b7c9c6
DESKTOP-1PF6GOH\chris <christian.kienle85@gmail.com>
2018-08-17 16:24:19 +0200

    Implement the first wizard page. #8

--- twisa-app/src/app/app.component.ts
+++ twisa-app/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    attributes: Array<Object> = [{id: 1, msg: 'Map'}, {id: 2, msg: 'Tweet-Text'}, {id: 3, msg: 'Source'}];
+    attributes: Array<Object> = [{id: 1, msg: 'Map', image: 'assets/images/Maps-icon.png'}, {id: 2, msg: 'Tweet-Text', image: 'assets/images/tweet-text.png'}, {id: 3, msg: 'Tweet-Source', image: 'assets/images/datasource.png'}];
     receivedData: Array<any> = [];
 
     transferDataSuccess($event: any) {
