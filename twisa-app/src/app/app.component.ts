1cceb9dc15774d89aaa8fd4934e0f2914a6119ba
DESKTOP-1PF6GOH\chris <christian.kienle85@gmail.com>
2018-08-23 11:20:12 +0200

    Add a header, footer and main section in HTML. #3

--- twisa-app/src/app/app.component.ts
+++ twisa-app/src/app/app.component.ts
@@ -6,12 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    attributes: Array<Object> = [{id: 1, msg: 'Map', image: 'assets/images/Maps-icon.png'}, {id: 2, msg: 'Tweet-Text', image: 'assets/images/tweet-text.png'}, {id: 3, msg: 'Tweet-Source', image: 'assets/images/datasource.png'}];
-    receivedData: Array<any> = [];
-
-    transferDataSuccess($event: any) {
-        this.receivedData.push($event);
-        // console.log($event);
-        console.log($event.dragData.msg);
-    }
+    title = 'TwiSA Web-App';
 }
