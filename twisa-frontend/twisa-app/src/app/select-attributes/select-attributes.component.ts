1500f022f57917d274d4b6e018cb654e4d63cf78
DESKTOP-3LLF12T\r4smus <christian.kienle85@googlemail.com>
2018-10-26 12:21:42 +0200

    Add new icon to the first wizard page for Created At #25

--- twisa-frontend/twisa-app/src/app/select-attributes/select-attributes.component.ts
+++ twisa-frontend/twisa-app/src/app/select-attributes/select-attributes.component.ts
@@ -28,6 +28,7 @@ export class SelectAttributesComponent {
         this.availableSelectAtts.push(SelectAttribute.TweetSource);
         this.availableSelectAtts.push(SelectAttribute.FollowerCount);
         this.availableSelectAtts.push(SelectAttribute.TweetsCount);
+        this.availableSelectAtts.push(SelectAttribute.CreatedAt);
         this.availableSelectAtts.push(SelectAttribute.Language);
         this.availableSelectAtts.push(SelectAttribute.Hashtag);
     }
