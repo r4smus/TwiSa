23b4f7623293c9e78a2b40bdbf35507cc82c6d0e
DESKTOP-1PF6GOH\chris <christian.kienle85@gmail.com>
2018-11-09 15:01:24 +0100

    Add addional languages #29

--- twisa-frontend/twisa-app/src/app/enums/language-type.ts
+++ twisa-frontend/twisa-app/src/app/enums/language-type.ts
@@ -4,7 +4,10 @@ export class LanguageType {
     static readonly DE = new LanguageType(1, 'German', 'assets/images/flags/flag_germany.png', 'de');
     static readonly EN = new LanguageType(2, 'English', 'assets/images/flags/flag_uk.png', 'en');
     static readonly RU = new LanguageType(3, 'Russian', 'assets/images/flags/flag_russia.png', 'ru');
-    static readonly ES = new LanguageType(3, 'Spanish', 'assets/images/flags/flag_spain.png', 'es');
+    static readonly ES = new LanguageType(4, 'Spanish', 'assets/images/flags/flag_spain.png', 'es');
+    static readonly FR = new LanguageType(5, 'French', 'assets/images/flags/flag_france.png', 'fr');
+    static readonly IT = new LanguageType(6, 'Italian', 'assets/images/flags/flag_italy.png', 'it');
+    static readonly PO = new LanguageType(7, 'Portuguese', 'assets/images/flags/flag_portugal.png', 'po');
 
     private constructor(public readonly id: number, public readonly name: string, public readonly imagePath: string, public readonly shortForm) {}
 
